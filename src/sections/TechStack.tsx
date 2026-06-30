fa028ca167a94e54f472a3b7fadc87fddc4c6eeb
Andrik <andrik27001@gmail.com>
2026-06-30 21:49:18 +0200

    Added stack logos

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -7,10 +7,9 @@ export const TechStack: React.FC = () => {
       <h2 className="text-[80px] text-nowrap font-semibold sticky top-50 w-[25%]">Tech Stack</h2>
       <div className="flex gap-10 flex-wrap">
         <TechCard logo="/public/images/TechLogos/html.png" title="HTML" />
-        <TechCard logo="/public/images/TechLogos/html.png" title="React" />
-        <TechCard logo="/public/images/TechLogos/html.png" title="React" />
-        <TechCard logo="/public/images/TechLogos/html.png" title="React" />
-        <TechCard logo="/public/images/TechLogos/html.png" title="React" />
+        <TechCard logo="/public/images/TechLogos/css.png" title="CSS" />
+        <TechCard logo="/public/images/TechLogos/ssms.png" title="SSMS" />
+        <TechCard logo="/public/images/TechLogos/react.png" title="React" />
       </div>
     </section>
   )
